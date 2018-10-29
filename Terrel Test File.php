50b92f528404daac76c8f1cf235307f3bacdc864
Terrel Stephen <terrel.stephen@dcmail.ca>
2018-10-29 13:37:12 -0400

    Update Terrel Test File.php
    Test file to make sure commits work.

---
+++ Terrel Test File.php
@@ -0,0 +1,31 @@
+<title>Beelzebub</title>
+
+<b>Beelzebub</b>- <p> One of the names given to the prince of all demons in his
+guise as Lord of the Flies.  The Greek demonologist Psellus says that
+the Hebrew Beelzebul (God of Heaven) was converted (as a playful
+corruption) into Beelzebub (God of the Dunghill), and the imaqe of this
+demons as a monstrous fly is derived from this notion of the dunghill,
+where flies proliferate.  According to some the name should be spelled
+"Baalzebub", but since this means "Lord of the High House" and might
+wrongly taken to be Solomon (in reference to his temple), the Jews
+therefore changed the name to "Lord of the Flies"
+
+These entmologies may be correct, but the notion of the Lord of the
+Flies is also classical, for it is recorded that in the temple of Actium
+sacrifices were made to Zeus under the name of Apomyios (meaning averter
+of flies). Pliny records these sacrifices were made to flies in Rome and
+Syrian temples: the later god was called Achor, and in some sources this
+god or demon is even called the God of the Flies. Some scholars say that
+Beelzebub was the equivalent of the Baal worshipped in Palestine and
+adjacent countries, and in the later times a representative of the
+fallen gods.  In Matthew, 12, 24 he is mentioned as "prince of the
+devils" and this appelation has stuck; for example Milton has him "next
+in crime" to Satan. (Paradice Lost, Book 1)
+
+The most striking image of Beelezbub is that of a huge fly given by
+Collin de Plancy, in which the wings are emblazoned with the death's
+head.  In the personal and historically unsound demonology of Barrett
+Beelzebub becomes the prince of false gods.  In the Michaelis Hierarchy
+Beelezbub is listed as the leader of sixteen demons, a fallen prince of
+Seraphim, and next unto Lucifer'; being the tempter through pride, his
+spiritual adversary is fittingly enough St. Fancis. </p>
