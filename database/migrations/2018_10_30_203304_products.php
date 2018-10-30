1199c22862356dd078e3f5c66580110a7c521b67
beattiek <kody.beattie@dcmail.ca>
2018-10-30 17:54:21 -0400

    renamed stuff

--- database/migrations/2018_10_30_203304_products.php
+++ database/migrations/2018_10_30_203304_products.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class OrderItems extends Migration
+class Products extends Migration
 {
     /**
      * Run the migrations.
