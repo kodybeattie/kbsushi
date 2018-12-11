9761e7abf7a715e6bcb870d32595c45ff0e84fec
beattiek <kody.beattie@dcmail.ca>
2018-12-11 11:02:04 -0500

    Added inventory management

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -19,15 +19,10 @@ class OrderController extends Controller
           ->select('users.phone_number','users.user_id','users.first_name','orders.datetime_ordered','order_products.product_id','order_products.quantity','products.product_name')
           ->orderBy('orders.datetime_ordered', 'asc')
           ->get();
-        //dd($users);
         $products = DB::table('products')
         ->join('order_products','order_products.product_id', '=', 'products.product_id')
         ->select('products.*')
         ->get();
-            //$cust = $users[0]->first_name;
-           //dd($products);
-      
-            
         return view('backend/orders',compact('users','products'));
     }
 
@@ -42,14 +37,35 @@ class OrderController extends Controller
 
       foreach ($products as $product_id => $info)
       {
-          //dd($products);
-          
         DB::table('order_products')->insert(['order_id' => $order->getKey(),
                                             'product_id' => $product_id,
                                             'quantity' => $info['quantity']]);
+        $ingredients = DB::table('order_products')
+            ->join('product_ingredients','order_products.product_id', '=', 'product_ingredients.product_id')
+            ->where('order_products.order_id', '=', $order->getKey())
+            ->select('product_ingredients.ing_id', 'product_ingredients.quantity AS prod_ing_qty',
+                     'order_products.quantity AS ord_prod_qty')
+            ->get();
+        foreach ($ingredients as $ing)
+        {
+          $deduction = ($ing->prod_ing_qty) * ($ing->ord_prod_qty);
+          $inventory_amount = DB::table('inventory')
+            ->select('quantity')
+            ->where('ing_id','=',$ing->ing_id)
+            ->get();
+
+          $new_amount = ($inventory_amount[0]->quantity) - $deduction;
+          DB::table('inventory')
+            ->where('ing_id','=',$ing->ing_id)
+            ->update(['quantity' => $new_amount]);
+
+        }
       }
+
+
+
       Session::forget('cart');
-      Session::flash('message', 'Order Complete, Please allow 15-20 mins for your food to be ready'); 
+      Session::flash('message', 'Order Complete, Please allow 15-20 mins for your food to be ready');
       return redirect()->home();
     }
 }
